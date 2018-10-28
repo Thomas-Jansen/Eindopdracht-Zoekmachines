41d48635301ed63111e70ca8053af4bba4db1b00
Thomas Jansen <36172862+Thomas-Jansen@users.noreply.github.com>
2018-10-28 21:52:00 +0100

    Update cleancsv.py

--- cleancsv.py
+++ cleancsv.py
@@ -1,18 +1,5 @@
-# import pandas as pd
-#
-# df = pd.read_csv("questions.csv")
-# titel = pd.DataFrame(["index"])
-# df = titel.append(df)
-#
-# df["index"].str().split(",", expand = True)
-#
-# for i in range(df.count()):
-#     item = df[i]
-#     if type(item["index"]) != int:
-#         if (item["index"] != '\\'):
-#             df[i-1].append(item["index"])
-#         else:
-#             df.drop(df.index[i])
+# A JSON-parser to read questions.csv and create manageable sized json files 
+
 
 import csv
 import json
@@ -40,22 +27,6 @@ jsonfile5 = open('questions5.json', 'w')
 fieldnames = ("Number","Date","UserId","Category", "Question", "Continue_1", "Continue_2", "Continue_3")
 reader = csv.DictReader( csvfile, fieldnames)
 
-
-
-# for row in reader:
-#     row["Question"] = clean(row["Question"])
-#     row["Continue_1"] = clean(row["Continue_1"])
-#     row["Continue_2"] = clean(row["Continue_2"])
-#     row["Continue_3"] = clean(row["Continue_3"])
-#     # print(row)
-#     # print('\n')
-#     json.dump(row, jsonfile)
-#     jsonfile.write(',')
-#     jsonfile.write('\n')
-# jsonfile.write(']}')
-
-
-# " " in row["Number"] or "/" in row["Number"] or "+" in row["Number"]
 def RepresentsInt(s):
     try:
         int(s)
@@ -142,15 +113,4 @@ for row in reader:
             # jsonfile.write(',')
             jsonfile5.write('\n')
     counter += 1
-#jsonfile.write('}')
 
-
-
-
-
-# jsonfile = open('questions.json', 'r')
-# jsonfile2 = open('questionsClean.json', 'w')
-#
-# for object in jsonfile:
-#     object = clean(object)
-#     json.dump(object, jsonfile)
